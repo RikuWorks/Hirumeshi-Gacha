82e9aca4c7ed20ef863970493959614116c7fa42
Motomochi <62926328+RikuWorks@users.noreply.github.com>
2024-01-30 18:10:21 +0900

    抽選機能とフィルタ機能を追加

--- HirumeshiGacha.rb
+++ HirumeshiGacha.rb
@@ -5,14 +5,28 @@ get '/' do
 end
 
 post '/gacha' do
+    meshiList = [["A", "1", "0","0","0"], ["B", "0", "1","0","0"],["C", "0", "0","1","0"],["D", "0", "0","0","1"]]
+    nmeshiList=[]
     @title = 'Todays Hirumeshi'
-    p params['EGG']
-    p params['SEA']
-    p params['BOU']
-    p params['JNK']
+    egg = params['EGG']
+    seafood = params['SEA']
+    bourgeois = params['BOU']
+    junky = params['JNK']
+    filter = [egg.to_i,seafood.to_i,bourgeois.to_i,junky.to_i]
+    p filter
+    meshiList.each do |p_a|
+      tf = p_a[1].to_i*filter[0]+p_a[2].to_i*filter[1]+p_a[3].to_i*filter[2]+p_a[4].to_i*filter[3]
+      if tf==0 then
+        nmeshiList.push(p_a)
+      end
+    end
+    p nmeshiList
+    list_size = nmeshiList.size
+    p list_size
+    meshi = rand(list_size)
+    @meshi = nmeshiList[meshi][0]
     erb :gacha
 end
-
 CHOICES = {
   'EGG' => 'たまごフィルタ',
   'SEA' => '海鮮フィルタ',
