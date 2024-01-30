e06c049a3d536a120c9286581c6749f81ae47452
Motomochi <62926328+RikuWorks@users.noreply.github.com>
2024-01-30 12:00:20 +0900

    チェックボックスから情報を受け取った

--- HirumeshiGacha.rb
+++ HirumeshiGacha.rb
@@ -5,24 +5,15 @@ get '/' do
     erb :index
 end
 
-post '/cast' do
-    @title = 'Thanks for casting your vote!'
-    @vote  = params['vote']
-    @store = YAML::Store.new 'votes.yaml'
-    @store.transaction do
-      @store['votes'] ||= {}
-      @store['votes'][@vote] ||= 0
-      @store['votes'][@vote] += 1
-    end
-    erb :cast
+post '/gacha' do
+    @title = 'Todays Hirumeshi'
+    p params['EGG']
+    p params['SEA']
+    p params['BOU']
+    p params['JNK']
+    erb :gacha
   end
-  
-  get '/results' do
-    @title = 'Results so far:'
-    @store = YAML::Store.new 'votes.yaml'
-    @votes = @store.transaction { @store['votes'] }
-    erb :results
-  end
+
 Choices = {
   'EGG' => 'たまごフィルタ',
   'SEA' => '海鮮フィルタ',
