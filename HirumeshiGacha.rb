c10f68cf3bcc1ed20cd1ab1119ab4193675cf325
Motomochi <62926328+RikuWorks@users.noreply.github.com>
2024-02-01 12:35:52 +0900

    動かなかったので動くようにした

--- HirumeshiGacha.rb
+++ HirumeshiGacha.rb
@@ -8,13 +8,13 @@ require "sinatra/json"
 use Rack::JSONBodyParser, verbs: ['POST']
 
 def conn
-  @conn || = PG.connect(dbname: 'postgres')
+  @conn ||= PG.connect(dbname: 'postgres')
 end
 
 configure do
   result = conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'hirumeshi'")
   conn.exec('CREATE TABLE hirumeshi (shop varchar(255), egg boolean,seafood boolean,bourgeois boolean,junky boolean)') if result.values.empty?
-  #conn.exec("INSERT INTO hirumeshi VALUES ('A',true,false,false,false);")
+  conn.exec("INSERT INTO hirumeshi VALUES ('A',true,false,false,false);")
 end
 
 get '/' do
@@ -37,10 +37,10 @@ post '/gacha' do
   bourgeois = params['BOU']
   junky = params['JNK']
   
-  sql_process = sql_process + egg_process if egg?
-  sql_process = sql_process + seafood_process if seafood?
-  sql_process = sql_process + bourgeois_process if bourgeois?
-  sql_process = sql_process + junky_process if junky?
+  sql_process = sql_process + egg_process if egg
+  sql_process = sql_process + seafood_process if seafood
+  sql_process = sql_process + bourgeois_process if bourgeois
+  sql_process = sql_process + junky_process if junky
   sql_process = sql_process + shop_process
   meshiList = conn.exec(sql_process)
   
@@ -48,7 +48,13 @@ post '/gacha' do
   nmeshiList = meshiList.values
   list_size = nmeshiList.size
   meshi = rand(list_size)
-  @meshi = nmeshiList[meshi][0]
+  temp = []
+  temp=" "
+  if list_size!=0
+      temp=nmeshiList[meshi][0]
+  end
+  @meshi = temp
+
   if mediaType == "application/json"
     data ={lunch:@meshi}
     json data
