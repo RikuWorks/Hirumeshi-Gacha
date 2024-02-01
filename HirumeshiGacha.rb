a20be04cff6b9d657e794bbdb244d56871811ac0
Motomochi <62926328+RikuWorks@users.noreply.github.com>
2024-02-01 10:02:49 +0900

    =付近の修正

--- HirumeshiGacha.rb
+++ HirumeshiGacha.rb
@@ -12,46 +12,46 @@ configure do
 end
 
 get '/' do
-    @title = 'Hirumeshi-Gacha'
-    erb :index
+  @title = 'Hirumeshi-Gacha'
+  erb :index
 end
 
 
 
 
 post '/gacha' do
-    sql_process = "SELECT shop FROM hirumeshi WHERE"
-    egg_process = " egg=false AND "
-    seafood_process = " seafood=false AND "
-    bourgeois_process = " bourgeois=false AND "
-    junky_process = " junky=false AND "
-    shop_process = " shop!=''"
+  sql_process = "SELECT shop FROM hirumeshi WHERE"
+  egg_process = " egg=false AND "
+  seafood_process = " seafood=false AND "
+  bourgeois_process = " bourgeois=false AND "
+  junky_process = " junky=false AND "
+  shop_process = " shop!=''"
 
-    egg = params['EGG']
-    seafood = params['SEA']
-    bourgeois = params['BOU']
-    junky = params['JNK']
-    if egg
-      sql_process=sql_process+egg_process
-    end
-    if seafood
-      sql_process=sql_process+seafood_process
-    end
-    if bourgeois
-      sql_process=sql_process+ bourgeois_process
-    end
-    if junky
-      sql_process=sql_process+junky_process
-    end
-    sql_process=sql_process+shop_process
-    meshiList=conn.exec(sql_process)
-    
-    @title = 'Todays Hirumeshi'
-    nmeshiList = meshiList.values
-    list_size = nmeshiList.size
-    meshi = rand(list_size)
-    @meshi = nmeshiList[meshi][0]
-    erb :gacha
+  egg = params['EGG']
+  seafood = params['SEA']
+  bourgeois = params['BOU']
+  junky = params['JNK']
+  if egg
+    sql_process = sql_process + egg_process
+  end
+  if seafood
+    sql_process = sql_process + seafood_process
+  end
+  if bourgeois
+    sql_process = sql_process + bourgeois_process
+  end
+  if junky
+    sql_process = sql_process + junky_process
+  end
+  sql_process = sql_process + shop_process
+  meshiList = conn.exec(sql_process)
+  
+  @title = 'Todays Hirumeshi'
+  nmeshiList = meshiList.values
+  list_size = nmeshiList.size
+  meshi = rand(list_size)
+  @meshi = nmeshiList[meshi][0]
+  erb :gacha
 end
 CHOICES = {
   'EGG' => 'たまごフィルタ',
